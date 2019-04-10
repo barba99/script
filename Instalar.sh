625981aea29eca73335868f4f578ac4b841eecdd
barba99 <38580791+barba99@users.noreply.github.com>
2019-04-09 21:43:39 -0500

    Create Instalar.sh

---
+++ Instalar.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+declare -A cor=( [0]="\033[1;37m" [1]="\033[1;34m" [2]="\033[1;32m" [3]="\033[1;36m" [4]="\033[1;31m" )
+barra="\033[0m\e[34m======================================================\033[1;37m"
+
+fun_bar () {
+comando[0]="$1"
+comando[1]="$2"
+ (
+[[ -e $HOME/fim ]] && rm $HOME/fim
+${comando[0]} -y > /dev/null 2>&1
+${comando[1]} -y > /dev/null 2>&1
+touch $HOME/fim
+ ) > /dev/null 2>&1 &
+echo -ne "\033[1;33m ["
+while true; do
+   for((i=0; i<18; i++)); do
+   echo -ne "\033[1;31m##"
+   sleep 0.1s
+   done
+   [[ -e $HOME/fim ]] && rm $HOME/fim && break
+   echo -e "\033[1;33m]"
+   sleep 1s
+   tput cuu1
+   tput dl1
+   echo -ne "\033[1;33m ["
+done
+echo -e "\033[1;33m]\033[1;31m -\033[1;32m 100%\033[1;37m"
+}
+
+instalar_fun () {
+cd /etc/adm-lite && bash cabecalho --instalar
+}
+
+text="Instalando"
+clear
+echo -e "${cor[3]}"
+read -n 1 -s -p "PRESIONE CUALQUIER TECLA PARA INICIAR LA INSTALACION..." ; echo "" ; echo "" ;
+
+
+
+echo -e "$barra"
+echo -e "${cor[2]} Update"
+fun_bar 'apt-get install screen' 'apt-get install python'
+echo -e "${cor[2]} Upgrade"
+fun_bar 'apt-get install lsof' 'apt-get install python3-pip'
+echo -e "${cor[2]} $text Lsof"
+fun_bar 'apt-get install python' 'apt-get install unzip'
+echo -e "${cor[2]} $text Python3"
+fun_bar 'apt-get install zip' 'apt-get install apache2'
+echo -e "${cor[2]} $text Unzip"
+fun_bar 'apt-get install ufw' 'apt-get install nmap'
+echo -e "${cor[2]} $text Screen"
+fun_bar 'apt-get install figlet' 'apt-get install bc'
+echo -e "${cor[2]} $text Figlet"
+fun_bar 'apt-get install lynx' 'apt-get install curl'
+echo -e "${cor[2]} $text complementos"
+fun_bar 'apt-get install ruby' 'apt install cowsay'
+gem install lolcat
+sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
+service apache2 restart > /dev/null 2>&1
+cowsay -f tux vps-barba |lolcat
